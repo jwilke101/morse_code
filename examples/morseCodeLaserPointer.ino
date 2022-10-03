392053c728092c64971ac9cb12ab1407520ca06e
jwilke101 <jwilke101@gmail.com>
2022-10-03 00:41:13 -0400

    Create morseCodeLaserPointer.ino

---
+++ examples/morseCodeLaserPointer.ino
@@ -0,0 +1,32 @@
+// Morse Code Laser Pointer
+// morseCodeLaserPointer.cpp 
+// Written by: Jeffrey A. Wilke
+// Copyright (C) 2022 Jeffrey A. Wilke
+// GNU GPL V3
+
+#include "morse_code.h"
+
+
+// morseCode mc("SOME MESSAGE", 10, 150);
+morseCode mc;
+
+void setup() {
+
+
+  _ss.begin(9600);
+  mc.pwmPin(10);
+  mc.defineUnit(150);  // 150 millis
+}
+
+void loop() {
+
+  mc.messageToEncode("SOME MESSAGE A TO Z 0 TO 9 -9.0 OK AR");
+  mc.playMessage();
+
+  // Plays: SOS LAT 99.99 LON 99.99 [USER MESSAGE] AR
+  mc.playGpsSos("MESSAGE TO APPEND");
+  
+  delay(5000);
+
+
+}
